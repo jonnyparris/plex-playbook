0cd9f9f1f09f9f35358214832ea6d651413e0fb0
jonnyparris <ruskin.constant+dev@gmail.com>
2021-05-19 16:36:14 +0100

    update host ip for remove_completed script

--- scripts/remove_completed.sh
+++ scripts/remove_completed.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-HOST=192.168.0.10:9091
+HOST=127.0.0.1:9091
 
 # use transmission-remote to get torrent list from transmission-remote list
 # use sed to delete first / last line of output, and remove leading spaces
 # use cut to get first field from each line
-TORRENTLIST=`transmission-remote $HOST --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited -d ' ' --fields=1`
+TORRENTLIST=$(transmission-remote $HOST --list | sed -e '1d;$d;s/^ *//' | cut --only-delimited -d ' ' --fields=1)
 # for each torrent in the list
-for TORRENTID in $TORRENTLIST
-do
-  TORRENTID=`echo $TORRENTID | sed 's:*::'`
+for TORRENTID in $TORRENTLIST; do
+  TORRENTID=$(echo $TORRENTID | sed 's:*::')
   # removes asterisk * from torrent ID# which had error associated with it
   echo "* * * * * Operations on torrent ID $TORRENTID starting. * * * * *"
   # check if torrent download is completed
-  DL_COMPLETED=`transmission-remote $HOST -t $TORRENTID -i | grep "Percent Done: 100%"`
+  DL_COMPLETED=$(transmission-remote $HOST -t $TORRENTID -i | grep "Percent Done: 100%")
   # check torrent’s current state is "Stopped", "Finished", or "Idle"
-  STATE_STOPPED=`transmission-remote $HOST -t $TORRENTID -i | grep "State: Stopped\|Finished\|Idle"`
+  STATE_STOPPED=$(transmission-remote $HOST -t $TORRENTID -i | grep "State: Stopped\|Finished\|Idle")
   # if the torrent is "Stopped", "Finished", or "Idle" after downloading 100%…
   if [ "$DL_COMPLETED" != "" ] && [ "$STATE_STOPPED" != "" ]; then
     # remove the torrent from Transmission and delete data.... data is linked to media dir anyways
